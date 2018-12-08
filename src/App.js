fa053c9f70bda6b6aad94ceb15875c25eadc5dc9
Hein Dauven <heindauven@gmail.com>
2018-12-08 21:49:24 +0100

    Add React root component and App component. App component binds everything together.

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import UserList from "./components/UserList";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false, msg: [] };
+  }
+
+  handleClick = api => e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+    fetch("/.netlify/functions/" + api)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.setState({ loading: false, msg: json.msg });
+      });
+  };
+
+  render() {
+    const { loading, msg } = this.state;
+
+    return (
+      <div className="App">
+        <button
+          className="btn btn-dark m-1"
+          onClick={this.handleClick("getusers")}
+        >
+          {loading ? "Loading..." : "Get Github users"}
+        </button>
+        <br />
+        <UserList users={msg} />
+      </div>
+    );
+  }
+}
+
+export default App;
